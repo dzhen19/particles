e13f3a8d2480f6cee9741208669bcbe4610cdd47
Derrick Zhen <derrickzhen@wc123-dhcp-76.brynmawr.edu>
2023-03-30 15:38:22 -0400

    completed billboard

--- shaders/simple-texture.vs
+++ shaders/simple-texture.vs
@@ -1,14 +1,26 @@
 #version 400
 
-layout (location = 0) in vec3 vPosition;
-layout (location = 1) in vec3 vNormal;
-layout (location = 2) in vec2 vUV;
+layout(location = 0) in vec3 vPosition;
+layout(location = 1) in vec3 vNormal;
+layout(location = 2) in vec2 vUV;
 
 uniform mat4 MVP;
+uniform vec3 eyePos;
 out vec2 uv;
 
-void main()
-{
+void main() {
   uv = vUV;
-  gl_Position = MVP * vec4(vPosition, 1.0); 
+
+  // vec3 n;
+  // n = (eyePos - vPosition) / distance(eyePos, vPosition);
+  // float theta;
+  // theta = atan(n[2], n[0]);
+
+  // mat3 rot;
+  // rot[0] = vec3(cos(theta), 0, sin(theta));
+  // rot[1] = vec3(0, 1, 0);
+  // rot[2] = vec3(-sin(theta), 0, cos(theta));
+
+  // gl_Position = MVP * vec4(rot * vPosition, 1.0);
+  gl_Position = MVP * vec4(vPosition, 1.0);
 }
--- src/billboard-axis.cpp
+++ src/billboard-axis.cpp
@@ -10,45 +10,88 @@ using namespace std;
 using namespace glm;
 using namespace agl;
 
-class Viewer : public Window {
+class Viewer : public Window
+{
 public:
-  Viewer() : Window() {
+  float radius = 10;
+  // in degrees [0, 360]
+  float azimuth = 0;
+  // in degrees [-90, 90]
+  float elevation = 0;
+
+  float scaleFactor = 1;
+
+  bool canRotate = false;
+
+  vec3 getEyePos()
+  {
+    return vec3(radius * sin(radians(azimuth)) * std::max(cos(radians(elevation)), float(0.01)),
+                radius * sin(radians(elevation)),
+                radius * cos(radians(azimuth)) * std::max(cos(radians(elevation)), float(0.01)));
   }
 
-  void setup() {
+  Viewer() : Window()
+  {
+  }
+
+  void setup()
+  {
     setWindowSize(1000, 1000);
     renderer.loadShader("simple-texture",
-      "../shaders/simple-texture.vs",
-      "../shaders/simple-texture.fs");
+                        "../shaders/simple-texture.vs",
+                        "../shaders/simple-texture.fs");
 
     Image img;
     img.load("../textures/tree.png", true);
+
+    scaleFactor = std::max((width() / img.width()), (height() / img.height())) ;
+
     renderer.loadTexture("tree", img, 0);
-    // TODO: Use the width and the height of the image to scale the billboard
 
     renderer.loadTexture("grass", "../textures/grass.png", 0);
     renderer.blendMode(agl::BLEND);
   }
 
+  void mouseMotion(int x, int y, int dx, int dy)
+  {
+    if (!canRotate)
+    {
+      return;
+    }
 
-  void mouseMotion(int x, int y, int dx, int dy) {
+    float newAzimuth = azimuth + dx;
+    azimuth = std::max(std::min(newAzimuth, float(360)), float(0));
+
+    float newElevation = elevation + dy;
+    elevation = std::max(std::min(newElevation, float(90)), float(-90));
   }
 
-  void mouseDown(int button, int mods) {
+  void mouseDown(int button, int mods)
+  {
+    canRotate = true;
   }
 
-  void mouseUp(int button, int mods) {
+  void mouseUp(int button, int mods)
+  {
+    canRotate = false;
   }
 
-  void scroll(float dx, float dy) {
+  void scroll(float dx, float dy)
+  {
+    float newRadius = radius += dy / 10;
+    radius = std::max(newRadius, float(.01));
   }
 
-  void draw() {
+  void draw()
+  {
     renderer.beginShader("simple-texture");
 
     float aspect = ((float)width()) / height();
     renderer.perspective(glm::radians(60.0f), aspect, 0.1f, 50.0f);
+    eyePos = getEyePos();
     renderer.lookAt(eyePos, lookPos, up);
+
+    renderer.scale(vec3(scaleFactor));
 
     // draw plane
     renderer.texture("Image", "grass");
@@ -62,20 +105,33 @@ public:
     renderer.texture("Image", "tree");
     renderer.push();
     renderer.translate(vec3(-0.5, -0.5, 0));
+
+    // rotate tree
+    vec3 n = normalize(eyePos - lookPos);
+    float theta_y = atan2(n.x, n.z);
+    renderer.push();
+    renderer.translate(vec3(0.5, 0.5, 0));
+    renderer.rotate(theta_y, vec3(0, 1, 0));
+    renderer.translate(vec3(-0.5, -0.5, 0));
+
+    // draw tree
     renderer.quad(); // vertices span from (0,0,0) to (1,1,0)
+
+    // pop off rotate
+    renderer.pop();
+    // pop off translate
     renderer.pop();
 
     renderer.endShader();
   }
 
 protected:
-
-  vec3 eyePos = vec3(0, 0, 2);
+  vec3 eyePos = vec3(1, 0, 0);
   vec3 lookPos = vec3(0, 0, 0);
   vec3 up = vec3(0, 1, 0);
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   Viewer viewer;
   viewer.run();
